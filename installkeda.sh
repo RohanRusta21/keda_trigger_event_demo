3f030a62a16facefd21759c6542b1140f2abaf08
Rohan Rustagi <110477025+RohanRusta21@users.noreply.github.com>
2024-07-17 15:18:46 +0530

    Update installkeda.sh

--- installkeda.sh
+++ installkeda.sh
@@ -8,22 +8,9 @@ helm repo update
 kubectl create ns keda
 helm install keda kedacore/keda --namespace keda
 
-# Add Prometheus Helm repository and update
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# Add Kafka Helm repository and update
+helm repo add strimzi https://strimzi.io/charts/
 helm repo update
 
-# Create namespace for Prometheus and install Prometheus
-kubectl create ns monitoring
-helm install prometheus prometheus-community/prometheus -n monitoring
-
-# Add Grafana Helm repository and update
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-
-# Create namespace for Grafana and install Grafana
-kubectl create ns grafana
-helm install grafana grafana/grafana -n grafana
-
-# Optional: Get the Grafana admin password
-echo "Grafana admin password:"
-kubectl get secret --namespace grafana grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+# Install kafka operator
+helm install strimzi-kafka-operator strimzi/strimzi-kafka-operator
